4cac9ec8218520dd05fc0f70e044d5cc4c83591a
Giordanobass <giordanoamaral@gmail.com>
2019-12-12 14:25:31 -0300

    Projeto Sistema

---
+++ src/main/java/com/algaworks/pedidovenda/converter/PedidoConverter.java
@@ -0,0 +1,40 @@
+package com.algaworks.pedidovenda.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.algaworks.pedidovenda.model.Pedido;
+import com.algaworks.pedidovenda.repository.Pedidos;
+
+@FacesConverter(forClass = Pedido.class)
+public class PedidoConverter implements Converter {
+
+	@Inject
+	private Pedidos pedidos;
+	
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		Pedido retorno = null;
+		
+		if (StringUtils.isNotEmpty(value)) {
+			Long id = new Long(value);
+			retorno = pedidos.porId(id);
+		}
+		
+		return retorno;
+	}
+
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if (value != null) {
+			Pedido pedido = (Pedido) value;
+			return pedido.getId() == null ? null : pedido.getId().toString();
+		}
+		
+		return "";
+	}
+
+}
